93449dbdc0fdadfd8e5f1111033f2b8b7fd4c41f
Jayson <jaysonwcs@gmail.com>
2020-06-17 21:17:44 -0300

    Exercise finished

---
+++ Entities/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_13.Entities
+{
+    class Item
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Item(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Exercise_13.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
 namespace Exercise_13
 {
@@ -6,7 +10,40 @@ namespace Exercise_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"D:\C Sharp\Exercise 13\";
+            string targetPath = @"D:\C Sharp\Exercise 13\out\";
+
+            List<Item> list = new List<Item>();
+
+            try
+            {
+                using StreamReader sr = File.OpenText(path + "in.csv");
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+
+                    list.Add(new Item(line[0], double.Parse(line[1], CultureInfo.InvariantCulture), int.Parse(line[2])));
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has ocurred:");
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                using StreamWriter sw = File.AppendText(targetPath + "summary.csv");
+                foreach (Item item in list)
+                {
+                    sw.WriteLine(item.Name + "," + (item.Price * item.Quantity).ToString(CultureInfo.InvariantCulture));
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has ocurred:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
